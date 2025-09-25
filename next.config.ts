f668018a540efbedae44808c5c16cde4bf1a6548
popularok123 <popularok123@gmail.com>
2025-09-25 11:16:37 +0800

    888

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+ 
 };
 
 export default nextConfig;
